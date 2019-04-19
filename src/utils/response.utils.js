92e505ef89d5e8d76977f92e5541d3457a2405d4
Miguel Rincon <miguel@miguels-mbp.home>
2019-04-19 11:43:19 -0400

    minor change

--- src/utils/response.utils.js
+++ src/utils/response.utils.js
@@ -4,16 +4,13 @@ const withStatusCode = (statusCode, formatter = null) => {
   }
 
   const hasFormatter = typeof formatter === "function";
-  // send whatever was passed in through if a formatter is not provided
   const format = hasFormatter ? formatter : _ => _;
 
-  // return a function that will take some data and formats a response with a status code
   return (data = null) => {
     const response = {
       statusCode: statusCode
     };
 
-    // only send a body if there is data
     if (data) {
       response.body = format(data);
     }
